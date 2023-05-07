ed71b87e6d8e7528d4d3db43d851c0b77ef6b484
Smolyanin Timofey <tim.smolyanin@gmail.com>
2023-05-07 22:53:43 +0300

    realized formation of the content of the network configuration

---
+++ network_configurator.py
@@ -0,0 +1,74 @@
+
+"""
+Настройки ethernet порта:
+- DHCP
+- Static IP
+
+Настройки Wi-Fi модема:
+- login/password
+
+Алгоритм:
+Когда пользователь осуществляет настройку сети, сначала осуществляется запись в
+конфигурационный файл введеных пользователем данных.
+Затем вызывается функция, которая считывает с конфигурационного файла настройки
+и которая записывает введенные настройки в /etc/network/interfaces файл.
+
+
+"""
+
+import yaml
+
+
+def network_configurator():
+    ETH0_DHCP_STRING = "auto eth0\n   iface eth0 inet dhcp"
+    ETH0_CONN_SETTINGS_STRING = ""
+    WIFI_CONN_SETTINGS_STRING = ""
+
+    with open("config.yaml") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    # WIFI
+    wifi_configs = config["network"]["wifi"]
+    eth_configs = config["network"]["eth0"]
+
+    if not wifi_configs["state"]:
+        WIFI_CONN_SETTINGS_STRING = "# Wi-Fi is not used"
+    else:
+        wifi_ssid = wifi_configs["ssid"]
+        wifi_password = wifi_configs["password"]
+        WIFI_CONN_SETTINGS_STRING = f"# Wireless interfaces wlan0\nauto wlan0\niface wlan0 inet dhcp\n" \
+                                    f"   wpa-ssid {wifi_ssid}\n   wpa-psk {wifi_password}"
+
+    # print(WIFI_CONN_SETTINGS_STRING)
+
+    # ETHERNET
+    eth_mode = eth_configs["mode"]
+    if eth_mode == "static":
+        eth0_ip = eth_configs["ip"]
+        eth0_mask = eth_configs["mask"]
+        eth0_gateway = eth_configs["gateway"]
+        if eth0_gateway == "None":
+            ETH0_CONN_SETTINGS_STRING = f"# Ethernet Port1 eth0\niface eth0 inet static\n   address {eth0_ip}\n" \
+                                f"   netmask {eth0_mask}"
+        else:
+            ETH0_CONN_SETTINGS_STRING = f"# Ethernet Port1 eth0\niface eth0 inet static\n   address {eth0_ip}\n" \
+                                    f"   netmask {eth0_mask}\n   gateway {eth0_gateway}"
+    elif eth_mode == "dhcp":
+        ETH0_CONN_SETTINGS_STRING = ETH0_DHCP_STRING
+
+    # print(ETH0_CONN_SETTINGS_STRING)
+
+    with open("C:/wk/korobka/network_config_template", "r") as f:
+        template = f.read()
+
+    network_settings = f"{template}{WIFI_CONN_SETTINGS_STRING}\n\n{ETH0_CONN_SETTINGS_STRING}"
+    return network_settings
+
+
+def test():
+    n = network_configurator()
+    print(n)
+
+
+if __name__ == "__main__":
+    test()
