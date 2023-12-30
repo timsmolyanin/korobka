93eadc5840511a9d9fedd1fc4f3d3cbbaf2a12b0
Smolyanin Timofey <tim.smolyanin@gmail.com>
2023-12-30 17:10:28 +0300

    update

---
+++ wb_virt_devices/electrol_heater_module.js
@@ -0,0 +1,15 @@
+defineVirtualDevice("ElectrolHeaterModule", {
+    title: "ElectrolHeaterModule",
+    cells: {
+      "Heater1 Setpoint": {
+      type: "value",
+      value: 0,
+      readonly: false
+      },
+      "Heater2 Setpoint": {
+      type: "value",
+      value: 0,
+      readonly: false
+      }
+    }
+  });
